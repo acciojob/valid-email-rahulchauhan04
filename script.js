1ec28538b8ba7cf78bb4341ac41bc3c45e8ddce5
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-28 21:45:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+	return emailPattern.test(str);
+	
 }
 
 // Do not change the code below.
